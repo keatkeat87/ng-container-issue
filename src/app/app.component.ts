63678369d51f9a8e454275541738011754734c20
兴杰 严 <keatkeat87@live.com>
2019-11-08 14:15:28 +0800

    ok

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, ViewChild, ViewContainerRef, TemplateRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  @ViewChild('container', { read: ViewContainerRef, static: true })
+  vcr: ViewContainerRef;
+
+  @ViewChild('template', { read: TemplateRef, static: true })
+  template: TemplateRef<any>;
+
+  click() {
+    this.vcr.createEmbeddedView(this.template, undefined);
+  }
+}
